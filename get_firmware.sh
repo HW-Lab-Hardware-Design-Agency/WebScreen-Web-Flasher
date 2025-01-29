53cac782c77dca3df1dcbba74357b07c6b31a674
andreock <andreockx@proton.me>
2025-01-29 01:32:11 +0100

    update script

--- get_firmware.sh
+++ get_firmware.sh
@@ -1,5 +1,5 @@
 BUILD_TYPE=$1
 
-mv ../firmware/esp32-s3-devkitc-1/* firmware/esp32-s3-devkitc-1/"$BUILD_TYPE"/
-mv ../firmware/arduino_nano_esp32/* firmware/arduino_nano_esp32/"$BUILD_TYPE"/
-mv ../firmware/lilygo_t_embed_cc1101/* firmware/lilygo_t_embed_cc1101/"$BUILD_TYPE"/
+mv firmware/esp32-s3-devkitc-1/* public/firmware/esp32-s3-devkitc-1/"$BUILD_TYPE"/
+mv firmware/arduino_nano_esp32/* public/firmware/arduino_nano_esp32/"$BUILD_TYPE"/
+mv firmware/lilygo_t_embed_cc1101/* public/firmware/lilygo_t_embed_cc1101/"$BUILD_TYPE"/
